e3eaeb312794b22a2a677e2a200b40ad0d874838
xingxerx <jemonemcc2@gmail.com>
2025-06-09 11:30:38 -0400

    Updated file: main.rs

---
+++ main.rs
@@ -0,0 +1,47 @@
+use std::ops::{Add, Sub, Mul};
+
+#[derive(Clone, Copy, Debug)]
+struct Vector2D {
+    x: f32,
+    y: f32,
+}
+
+impl Vector2D {
+    fn new(x: f32, y: f32) -> Self {
+        Vector2D { x, y }
+    }
+
+    fn magnitude(&self) -> f32 {
+        (self.x * self.x + self.y * self.y).sqrt()
+    }
+
+    fn normalize(&self) -> Self {
+        let mag = self.magnitude();
+        if mag == 0.0 {
+            Vector2D { x: 0.0, y: 0.0 }
+        } else {
+            Vector2D { x: self.x / mag, y: self.y / mag }
+        }
+    }
+}
+
+impl Add for Vector2D {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        Vector2D { x: self.x + other.x, y: self.y + other.y }
+    }
+}
+
+impl Sub for Vector2D {
+    type Output = Self;
+    fn sub(self, other: Self) -> Self {
+        Vector2D { x: self.x - other.x, y: self.y - other.y }
+    }
+}
+
+impl Mul<f32> for Vector2D {
+    type Output = Self;
+    fn mul(self, scalar: f32) -> Self {
+        Vector2D { x: self.x * scalar, y: self.y * scalar }
+    }
+}
