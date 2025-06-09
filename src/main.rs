846313dad53aa89750733334e02bfdee16aa6bff
xingxerx <jemonemcc2@gmail.com>
2025-06-09 12:14:07 -0400

    Updated 30 files: Cargo.toml, main.rs, bin-epoch_of_elria, output-bin-epoch_of_elria, epoch_of_elria-6f811a196853e733, 0h1ktz6xr6npikiy4zilghdtt.o, 0hahvgqxcxwvekgi4i6hm6w6g.o, 0jorrocm3zozjd1avkx42vyn3.o, 0u9fys4ck9efbvqei6siwj0f5.o, 1ezf7bxqwac85abjb05qznseu.o, 1mtg8tkdlin7y2v2kpsklu5lh.o, 1y98bajiqg2i6j4s72d0wlg9n.o, 23cwmrosubeqzv8gocomeeqz9.o, 2t9mptxv4owevz0me1h7kezgc.o, 7gbmn81ptbabc6cw5347nqlvk.o, 98c8xys8kwmnoo247yur03red.o, 99stxya9ah6k1ymv9zwh61g3d.o, 9dxy7we9eqrpimr0cvn4e4nef.o, ay5o9aesby25ts5h93afune5w.o, aybo9ur4hzath53x5zmi1ozkh.o, bxd9f2fa73b1p86dy771pnxoe.o, dep-graph.bin, dpmb0kpctqolbqlmvprqi4ewk.o, edvqa3gah9jrqjvnjnclw7s5j.o, ellxm2lwfcjpopcyu8jg59u35.o, ev37p8g8nnm7z0veahg9108r2.o, ewymjxbubn9wv0kisjfgzwex4.o, query-cache.bin, work-products.bin, dep-graph.part.bin Added 25 files: epoch_of_elria-6f811a196853e733, 0h1ktz6xr6npikiy4zilghdtt.o, 0hahvgqxcxwvekgi4i6hm6w6g.o, 0jorrocm3zozjd1avkx42vyn3.o, 0u9fys4ck9efbvqei6siwj0f5.o, 1ezf7bxqwac85abjb05qznseu.o, 1mtg8tkdlin7y2v2kpsklu5lh.o, 1y98bajiqg2i6j4s72d0wlg9n.o, 23cwmrosubeqzv8gocomeeqz9.o, 2t9mptxv4owevz0me1h7kezgc.o, 7gbmn81ptbabc6cw5347nqlvk.o, 98c8xys8kwmnoo247yur03red.o, 99stxya9ah6k1ymv9zwh61g3d.o, 9dxy7we9eqrpimr0cvn4e4nef.o, ay5o9aesby25ts5h93afune5w.o, aybo9ur4hzath53x5zmi1ozkh.o, bxd9f2fa73b1p86dy771pnxoe.o, dep-graph.bin, dpmb0kpctqolbqlmvprqi4ewk.o, edvqa3gah9jrqjvnjnclw7s5j.o, ellxm2lwfcjpopcyu8jg59u35.o, ev37p8g8nnm7z0veahg9108r2.o, ewymjxbubn9wv0kisjfgzwex4.o, query-cache.bin, work-products.bin

--- src/main.rs
+++ src/main.rs
@@ -1,152 +1,56 @@
-use std::ops::{Add, Sub, Mul};
+use minifb::{Key, Window, WindowOptions};
 
-use std::fmt::Write as FmtWrite;
-
-#[derive(Clone, Copy, Debug)]
-struct Vector2D {
-    x: f32,
-    y: f32,
-}
-
-#[allow(dead_code)]
-impl Vector2D {
-    #[allow(dead_code)]
-    fn new(x: f32, y: f32) -> Self {
-        Vector2D { x, y }
-    }
-
-    fn magnitude(&self) -> f32 {
-        (self.x * self.x + self.y * self.y).sqrt()
-    }
-
-    fn normalize(&self) -> Self {
-        let mag = self.magnitude();
-        if mag == 0.0 {
-            Vector2D { x: 0.0, y: 0.0 }
-        } else {
-            Vector2D { x: self.x / mag, y: self.y / mag }
-        }
-    }
-}
-
-impl Add for Vector2D {
-    type Output = Self;
-    fn add(self, other: Self) -> Self {
-        Vector2D { x: self.x + other.x, y: self.y + other.y }
-    }
-}
-
-impl Sub for Vector2D {
-    type Output = Self;
-    fn sub(self, other: Self) -> Self {
-        Vector2D { x: self.x - other.x, y: self.y - other.y }
-    }
-}
-
-impl Mul<f32> for Vector2D {
-    type Output = Self;
-    fn mul(self, scalar: f32) -> Self {
-        Vector2D { x: self.x * scalar, y: self.y * scalar }
-    }
-}
-
-trait GameObject {
-    fn update(&mut self);
-    fn draw(&self, svg: &mut String);
-    fn get_position(&self) -> Vector2D;
-    fn get_size(&self) -> (f32, f32);
-
-    // Axis-Aligned Bounding Box collision
-    fn collides_with(&self, other: &dyn GameObject) -> bool {
-        let (x1, y1) = (self.get_position().x, self.get_position().y);
-        let (w1, h1) = self.get_size();
-        let (x2, y2) = (other.get_position().x, other.get_position().y);
-        let (w2, h2) = other.get_size();
-
-        x1 < x2 + w2 && x1 + w1 > x2 && y1 < y2 + h2 && y1 + h1 > y2
-    }
-}
-
-struct Player {
-    position: Vector2D,
-    velocity: Vector2D,
-    width: f32,
-    height: f32,
-}
-
-impl Player {
-    fn new(position: Vector2D) -> Self {
-        Player {
-            position,
-            velocity: Vector2D::new(2.0, 0.0), // Moves right
-            width: 40.0,
-            height: 40.0,
-        }
-    }
-}
-
-impl GameObject for Player {
-    fn update(&mut self) {
-        self.position = self.position + self.velocity;
-    }
-
-    fn draw(&self, svg: &mut String) {
-        let _ = FmtWrite::write_fmt(
-            svg,
-            format_args!(
-                r#"<rect x="{}" y="{}" width="{}" height="{}" fill="blue" />"#,
-                self.position.x, self.position.y, self.width, self.height
-            ),
-        );
-    }
-
-    fn get_position(&self) -> Vector2D {
-        self.position
-    }
-
-    fn get_size(&self) -> (f32, f32) {
-        (self.width, self.height)
-    }
-}
+const WIDTH: usize = 800;
+const HEIGHT: usize = 600;
 
 fn main() {
-    println!("Epoch of Elria game engine (Rust) starting up!");
+    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+    let mut window = Window::new(
+        "Epoch of Elria",
+        WIDTH,
+        HEIGHT,
+        WindowOptions::default(),
+    ).unwrap_or_else(|e| {
+        panic!("Failed to create window: {}", e);
+    });
 
-    // Create two players at different positions
-    let mut player1 = Player::new(Vector2D::new(200.0, 0.0));
-    let mut player2 = Player::new(Vector2D::new(0.0, 0.0));
-    let mut player3 = Player {
-        position: Vector2D::new(100.0, 0.0),
-        velocity: Vector2D::new(-2.0, 0.0),
-        width: 40.0,
-        height: 40.0,
-    };
+    // Set a maximum FPS to not run too fast
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
 
-    // Game loop for 120 frames
-    for frame in 0..120 {
-        println!("Frame {}", frame);
-        player1.update();
-        player2.update();
-        player3.update();
+    let mut frame_count = 0;
+    let mut positions = [(202, 0), (2, 0), (98, 0)];
+    let rect_size = 40;
 
-        // Draw to SVG string (for demonstration)
-        let mut svg = String::from(
-            r#"<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"800\" height=\"600\">"#,
-        );
-        player1.draw(&mut svg);
-        player2.draw(&mut svg);
-        player3.draw(&mut svg);
-        svg.push_str("</svg>");
+    while window.is_open() && !window.is_key_down(Key::Escape) && frame_count < 120 {
+        // Clear buffer
+        for pixel in buffer.iter_mut() {
+            *pixel = 0;
+        }
 
-        println!("{}", svg);
+        // Draw rectangles
+        for (x, y) in positions.iter() {
+            for dy in 0..rect_size {
+                for dx in 0..rect_size {
+                    let px = *x + dx;
+                    let py = *y + dy;
+                    if px < WIDTH && py < HEIGHT {
+                        buffer[py * WIDTH + px] = 0x0000FF;  // Blue color
+                    }
+                }
+            }
+        }
 
-        // Check for collisions between all pairs
-        let collision_1_2 = player1.collides_with(&player2);
-        let collision_1_3 = player1.collides_with(&player3);
-        let collision_2_3 = player2.collides_with(&player3);
-        println!(
-            "Collisions: 1-2: {}, 1-3: {}, 2-3: {}",
-            collision_1_2, collision_1_3, collision_2_3
-        );
+        // Update positions
+        positions[0].0 += 2;  // First rectangle moves right
+        positions[1].0 += 2;  // Second rectangle moves right
+        positions[2].0 -= 2;  // Third rectangle moves left
+
+        // Update window
+        window.update_with_buffer(&buffer, WIDTH, HEIGHT)
+            .unwrap_or_else(|e| {
+                panic!("Failed to update window: {}", e);
+            });
+
+        frame_count += 1;
     }
 }
