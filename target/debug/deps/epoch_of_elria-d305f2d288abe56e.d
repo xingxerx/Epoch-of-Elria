b2819da86670f84ab67347ea1697bb439d8d10f4
xingxerx <jemonemcc2@gmail.com>
2025-06-10 02:08:55 -0400

    Updated 15 files: .rustc_info.json, bin-epoch_of_elria, example-complete_rpg_demo, output-example-complete_rpg_demo, lib-epoch_of_elria, output-lib-epoch_of_elria, epoch_of_elria-d305f2d288abe56e.d, complete_rpg_demo-bf4cf550f878d3df.d, dep-graph.part.bin, s-h85plowkd5-05ym04o.lock, dep-graph.bin, dep-graph.part.bin, query-cache.bin, work-products.bin, s-h85pltuku5-1vyip4j.lock

--- target/debug/deps/epoch_of_elria-d305f2d288abe56e.d
+++ target/debug/deps/epoch_of_elria-d305f2d288abe56e.d
@@ -1,6 +1,6 @@
-/mnt/f/Epoch of Elria/target/debug/deps/libepoch_of_elria-d305f2d288abe56e.rmeta: src/lib.rs src/math.rs src/game_objects.rs src/rendering.rs src/input.rs src/physics.rs src/audio.rs src/scene.rs
+/mnt/f/Epoch of Elria/target/debug/deps/libepoch_of_elria-d305f2d288abe56e.rmeta: src/lib.rs src/math.rs src/game_objects.rs src/rendering.rs src/input.rs src/physics.rs src/audio.rs src/scene.rs src/game_framework.rs src/advanced_rendering.rs src/game_templates.rs
 
-/mnt/f/Epoch of Elria/target/debug/deps/epoch_of_elria-d305f2d288abe56e.d: src/lib.rs src/math.rs src/game_objects.rs src/rendering.rs src/input.rs src/physics.rs src/audio.rs src/scene.rs
+/mnt/f/Epoch of Elria/target/debug/deps/epoch_of_elria-d305f2d288abe56e.d: src/lib.rs src/math.rs src/game_objects.rs src/rendering.rs src/input.rs src/physics.rs src/audio.rs src/scene.rs src/game_framework.rs src/advanced_rendering.rs src/game_templates.rs
 
 src/lib.rs:
 src/math.rs:
@@ -10,5 +10,8 @@ src/input.rs:
 src/physics.rs:
 src/audio.rs:
 src/scene.rs:
+src/game_framework.rs:
+src/advanced_rendering.rs:
+src/game_templates.rs:
 
 # env-dep:CARGO_PKG_VERSION=0.1.0
---
+++ target/debug/examples/complete_rpg_demo-bf4cf550f878d3df.d
@@ -0,0 +1,5 @@
+F:\Epoch of Elria\target\debug\examples\libcomplete_rpg_demo-bf4cf550f878d3df.rmeta: examples\complete_rpg_demo.rs
+
+F:\Epoch of Elria\target\debug\examples\complete_rpg_demo-bf4cf550f878d3df.d: examples\complete_rpg_demo.rs
+
+examples\complete_rpg_demo.rs:
