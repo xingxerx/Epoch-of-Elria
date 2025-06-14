5202ee170e4c7c942384eb713fb959295ebf2ae8
xingxerx <jemonemcc2@gmail.com>
2025-06-13 23:28:43 -0400

    Updated 2 files: output-lib-epoch_of_elria, epoch_of_elria-4ff7753768f50076.d

---
+++ target/debug/deps/epoch_of_elria-4ff7753768f50076.d
@@ -0,0 +1,19 @@
+/mnt/f/Epoch of Elria/target/debug/deps/libepoch_of_elria-4ff7753768f50076.rmeta: src/lib.rs src/math.rs src/game_objects.rs src/rendering.rs src/input.rs src/physics.rs src/audio.rs src/scene.rs src/game_framework.rs src/advanced_rendering.rs src/game_templates.rs
+
+/mnt/f/Epoch of Elria/target/debug/deps/libepoch_of_elria-4ff7753768f50076.rlib: src/lib.rs src/math.rs src/game_objects.rs src/rendering.rs src/input.rs src/physics.rs src/audio.rs src/scene.rs src/game_framework.rs src/advanced_rendering.rs src/game_templates.rs
+
+/mnt/f/Epoch of Elria/target/debug/deps/epoch_of_elria-4ff7753768f50076.d: src/lib.rs src/math.rs src/game_objects.rs src/rendering.rs src/input.rs src/physics.rs src/audio.rs src/scene.rs src/game_framework.rs src/advanced_rendering.rs src/game_templates.rs
+
+src/lib.rs:
+src/math.rs:
+src/game_objects.rs:
+src/rendering.rs:
+src/input.rs:
+src/physics.rs:
+src/audio.rs:
+src/scene.rs:
+src/game_framework.rs:
+src/advanced_rendering.rs:
+src/game_templates.rs:
+
+# env-dep:CARGO_PKG_VERSION=0.1.0
